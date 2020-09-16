55e3181ef5319a7898c8d79ac8c92cc4830001ca
Seonghwa Yun <seonghwa.yun@comcom.ai>
2020-09-16 17:03:38 +0900

    remove wallet module

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import Client from './models/client';
-import Wallet from './models/wallet';
 import Worker from './models/worker';
 import * as types from './common/types';
 import * as error from './common/error';
 
 export {
-  Client, Wallet, Worker, types, error,
+  Client, Worker, types, error,
 };
