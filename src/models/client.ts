f7bca6edc9ecbd23c0570ce92f4985c1de54e6e6
Seonghwa Yun <seonghwa.yun@comcom.ai>
2020-10-29 19:20:41 +0900

    fix typo

--- src/models/client.ts
+++ src/models/client.ts
@@ -161,7 +161,7 @@ export default class Client {
           updatedAt: value.updatedAt,
           clusterName: status.clusterName,
           type: status.type,
-          nodePoo: targetNodePool,
+          nodePool: targetNodePool,
         });
       }
     }
